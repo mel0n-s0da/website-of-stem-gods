8a41934940760853965886c4ba35715b98954c0b
mel0n-s0da <91900981+mel0n-s0da@users.noreply.github.com>
2021-12-20 13:46:18 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+peopleLastName = ['fleming', 'galvani', 'meitner', 'orsted', 'turing']
+
+
+// find the name from peopleLastName, store as an index
+// display image and name on the site based on that  
+
