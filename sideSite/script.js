90710800e7fe5aaec37a8f887e95dee2798dbb1f
mel0n-s0da <91900981+mel0n-s0da@users.noreply.github.com>
2021-12-24 10:26:30 -0500

    Update script.js

--- sideSite/script.js
+++ sideSite/script.js
@@ -1,15 +1,34 @@
+nameKey = ['fleming', 'galvani', 'meitner', 'orsted', 'turing'];
+fullNames = ['Alexander Fleming', 'Luigi Galvani', 'Elise Meitner', 'Hans Christian Orsted', 'Alan Mathison Turing']; 
+
 var topDisplay = document.getElementById('nameDisplay');
+var imageDisplay = document.getElementById('imageDisplay');
 var faceBlocks = Array.from(document.getElementsByClassName('face-block'));
 // console.log(faceBlocks);
 
 var name = ''
 faceBlocks.map( block => {
-    block.addEventListener('click', (e) => {
+    block.addEventListener('mouseover', (e) => {
         // the key system to assign each click to a last name 
         var type = e.target.nodeName; 
         var text = e.target.innerText; 
         var key; 
-        //console.log(type);
+        if ((type == "IMG") || (type == "DIV"))  {
+          key = e.target.id;
+        } else if ((type == "B") || (type == "CENTER")) {
+          key = (((text.split(" "))).at(-1)).toLowerCase();
+        } else {
+          key = "yay life"; 
+        }
+        // display the name of the person 
+        personName = fullNames[nameKey.indexOf(key)];
+        topDisplay.innerText = personName; 
+        // display the image of the person 
+        var img = document.createElement("IMG");
+        img.src = "images/" + key + '.png';
+        imageDisplay.replaceChild(img, imageDisplay.childNodes[0]);
+
+        /*
         switch (type) {
           case "IMG":
             key = e.target.id; 
@@ -26,10 +45,9 @@ faceBlocks.map( block => {
           default:
             console.log("yay life");
         } 
+        */
         //console.log(key);
-        topDisplay.innerText = key;
         // parallel array time 
 
     });
 });
-
