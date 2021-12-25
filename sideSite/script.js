2302ebf8d1c06462924793c973ad8cb65a0d52b8
mel0n-s0da <91900981+mel0n-s0da@users.noreply.github.com>
2021-12-25 11:49:12 -0500

    Update script.js

--- sideSite/script.js
+++ sideSite/script.js
@@ -1,14 +1,21 @@
 nameKey = ['fleming', 'galvani', 'meitner', 'orsted', 'turing'];
 fullNames = ['Alexander Fleming', 'Luigi Galvani', 'Elise Meitner', 'Hans Christian Orsted', 'Alan Mathison Turing']; 
+textPeople = ["this kid did penicillin", "this kid did electricity stuff", "this kid did similar stuff to curie and should've gotten the nobel prize and was the person to be nominated the most times and never recieve one and refused to work on the manhattan project", "this kid did e & m", "this kid isn't the first computer scientist"]; 
 
+var backgroundDisplay = document.getElementById('displayTogether');
 var topDisplay = document.getElementById('nameDisplay');
 var imageDisplay = document.getElementById('imageDisplay');
+var textDisplay = document.getElementById('textDisplay');
 var faceBlocks = Array.from(document.getElementsByClassName('face-block'));
 // console.log(faceBlocks);
 
+var currentPerson = '';
+
 var name = ''
 faceBlocks.map( block => {
-    block.addEventListener('mouseover', (e) => {
+    block.addEventListener('click', (e) => {
+        // change the background color to grey 
+        backgroundDisplay.style.backgroundColor = 'lightgrey';
         // the key system to assign each click to a last name 
         var type = e.target.nodeName; 
         var text = e.target.innerText; 
@@ -20,13 +27,20 @@ faceBlocks.map( block => {
         } else {
           key = "yay life"; 
         }
+        // key index 
+        let keyIndex = nameKey.indexOf(key);
         // display the name of the person 
-        personName = fullNames[nameKey.indexOf(key)];
+        personName = fullNames[keyIndex];
         topDisplay.innerText = personName; 
         // display the image of the person 
         var img = document.createElement("IMG");
         img.src = "images/" + key + '.png';
         imageDisplay.replaceChild(img, imageDisplay.childNodes[0]);
+        // display the text with the info of the person
+        var info = document.createElement("P");
+        var infoText = textPeople[keyIndex];
+        info.innerHTML = infoText;  
+        textDisplay.replaceChild(info, textDisplay.childNodes[1])
 
         /*
         switch (type) {
@@ -51,3 +65,4 @@ faceBlocks.map( block => {
 
     });
 });
+
